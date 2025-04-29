dc72f4b8589d1655a15443b92669d534f20dbce6
sanah <sanahbhimani@gmail.com>
2025-04-29 14:43:52 -0400

    create a docs directory for sphinx documentation setup. initial stages begun with editing the conf.py to point to where the documented code is as well as some autodoc default settings and extensions settings

---
+++ docs/source/conf.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('/Users/sanuma/git/MetaLens/MetaLens'))
+
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MetaLens'
+copyright = '2025, Sanah A. Bhimani'
+author = 'Sanah A. Bhimani'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napolean',]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'private-members': False,
+    'special-members': '__init__',
+    'inherited-members': True,
+    'show-inheritance': True,
+}
+
+
+language = 'y'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
