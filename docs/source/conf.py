37cb02ee99798fe6862200f5516e6a937a2692ff
sanah <sanahbhimani@gmail.com>
2025-04-29 15:14:24 -0400

    update conf.py to use readthedocs layout similar to our SO ones

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,8 @@ release = '0.1'
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',]
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -42,5 +43,5 @@ language = 'y'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
