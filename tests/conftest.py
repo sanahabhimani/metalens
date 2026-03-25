4b238eb80b93eb8d1c3f8562d2fcd9e697546f51
sanah <sanahbhimani@gmail.com>
2026-03-25 14:19:48 -0500

    starting the framework of getting some tests setup

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+# make fixtures for:
+# fake_cutparams_file
+# fake spindle cal file
+# fake metrology file plane
+# fake config yaml files (3 -- spindles.yaml, lenstesttouches.yaml, lensparams.yaml)
